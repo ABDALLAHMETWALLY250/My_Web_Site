2fd9bf82aa0cf0b8bf5fadfeedbc3b1f9c38e0c0
Abdallah Mohamed <105307151+ABDALLAHMETWALLY250@users.noreply.github.com>
2023-10-02 11:00:31 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,18 @@ import HomeView from "../views/HomeView.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/about",
-    name: "about",
+    name: "About",
+    meta: {
+      title: "About",
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -18,7 +24,10 @@ const routes = [
   },
   {
     path: "/services",
-    name: "services",
+    name: "Services",
+    meta: {
+      title: "Services",
+    },
     // route level code-splitting
     // this generates a separate chunk (services.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -27,7 +36,10 @@ const routes = [
   },
   {
     path: "/skills",
-    name: "skills",
+    name: "Skills",
+    meta: {
+      title: "Skills",
+    },
     // route level code-splitting
     // this generates a separate chunk (skills.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -36,7 +48,10 @@ const routes = [
   },
   {
     path: "/works",
-    name: "works",
+    name: "Works",
+    meta: {
+      title: "Works",
+    },
     // route level code-splitting
     // this generates a separate chunk (works.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -45,7 +60,10 @@ const routes = [
   },
   {
     path: "/contact",
-    name: "contact",
+    name: "Contact",
+    meta: {
+      title: "Contact",
+    },
     // route level code-splitting
     // this generates a separate chunk (contact.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -55,6 +73,9 @@ const routes = [
   {
     path: "/:pathMatch(.*)*",
     name: "erorrpage",
+    meta: {
+      title: "404 Page Not Found",
+    },
     // route level code-splitting
     // this generates a separate chunk (contact.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -66,6 +87,10 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from) => {
+  document.title = `${to.meta?.title} - ABDALLAH` ?? "ABDALLAH";
 });
 
 export default router;
